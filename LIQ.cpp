35f7a5af377169fc5f6b2ffe2752cebebb9015c7
Dat <nguyenvandat511999@gmail.com>
2019-03-16 16:08:34 +0700

    VNOI

--- LIQ.cpp
+++ LIQ.cpp
@@ -7,7 +7,6 @@ int main()
 	f[0]=1;
 	int max=0;
 	for (int i = 0; i < N; i++)
-<<<<<<< HEAD
 		cin>>a[i];
 	for (int i = 1; i < N; ++i)
 	{
@@ -21,16 +20,5 @@ int main()
 		if(f[i]>max)
 			max = f[i];
 	cout<<max<<endl;
-=======
-	{
-		cin>>a[i];
-	}
-	for (int j = 1; j < N; j++)
-			if(a[j]>a[j-1])
-				f[j] =  f[j-1]+1;
-			else
-				f[j] = f[j-1];
-	cout<<f[N-1]<<endl;
->>>>>>> 73289fb48ce8146b50fe23c8326d0ed6adc84e6b
 	return 0;
 }
